f420b36a5588d1a9316b1c84a405b6e15a3c81f6
Boming Zhang <bz14520@uga.edu>
2024-01-24 23:52:20 -0500

    fix: IPv4 loopback address

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
       },
       '/swagger': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
       }
     }
