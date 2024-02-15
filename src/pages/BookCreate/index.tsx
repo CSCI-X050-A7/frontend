261d8ccf9f79d380f71c8f5445eb3409fec7e559
Boming Zhang <bz14520@uga.edu>
2024-02-15 16:03:46 -0500

    refactor: remove unnecessary components

--- src/pages/BookCreate/index.tsx
+++ src/pages/BookCreate/index.tsx
@@ -1,10 +1,65 @@
-import BookCreate from 'components/BookCreate'
+import { useRequest } from 'ahooks'
 import PageContainer from 'components/PageContainer'
+import { useAuth } from 'hooks/useAuth'
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import { useNavigate } from 'react-router-dom'
+import Backend from 'utils/service'
 
-const Index: React.FC = () => (
-  <PageContainer>
-    <BookCreate />
-  </PageContainer>
-)
+const Index: React.FC = () => {
+  const { user } = useAuth()
+  const navigate = useNavigate()
+  const [bookTitle, setBookTitle] = useState('my book')
+  const { run: create } = useRequest(
+    async () =>
+      Backend.book.v1BooksCreate({
+        author: 'string',
+        meta: {
+          description: 'string',
+          picture: 'string',
+          rating: 10
+        },
+        status: 0,
+        title: bookTitle,
+        user_id: user?.id ?? ''
+      }),
+    {
+      manual: true,
+      onSuccess: () => {
+        navigate('/')
+      }
+    }
+  )
+  return (
+    <PageContainer>
+      <div className='text-center'>
+        <h1>Create Book</h1>
+      </div>
+      <Form>
+        <Form.Group className='mb-3'>
+          <Form.Label>Book Title</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Book title'
+            defaultValue={bookTitle}
+            onChange={e => {
+              setBookTitle(e.target.value)
+            }}
+          />
+        </Form.Group>
+        <Button
+          variant='primary'
+          type='button'
+          onClick={(): void => {
+            create()
+          }}
+        >
+          Create
+        </Button>
+      </Form>
+    </PageContainer>
+  )
+}
 
 export default Index
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,45 @@
-import Book from 'components/Book'
+import { useRequest } from 'ahooks'
 import PageContainer from 'components/PageContainer'
+import type React from 'react'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Backend from 'utils/service'
 
-const Index: React.FC = () => (
-  <PageContainer>
-    <Book />
-  </PageContainer>
-)
+const Index: React.FC = () => {
+  const { data, loading } = useRequest(async () => Backend.book.v1BooksList())
+  return (
+    <PageContainer>
+      <div className='text-center'>
+        <h1>Vite + React + React-Bootstrap Boilerplate</h1>
+        <p>
+          Visit backend swagger: <a href='/swagger'>here</a>
+        </p>
+      </div>
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center'
+        }}
+      >
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>
+              <b>{loading ? 'loading...' : 'Books'}</b>
+            </Card.Title>
+          </Card.Body>
+          {!loading && (
+            <ListGroup variant='flush'>
+              {data?.data.data?.map(book => (
+                <ListGroup.Item key={book.id}>{book.title}</ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Card>
+      </div>
+    </PageContainer>
+  )
+}
 
 export default Index
