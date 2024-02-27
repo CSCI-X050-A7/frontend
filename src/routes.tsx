aaaced598cb4383105d75099e4ec57680787569c
pdp32147 <pdp32147@uga.edu>
2024-02-27 17:04:05 -0500

    quick fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,13 @@
 import { ProtectedRoute } from 'components/ProtectedRoute'
+import Checkout from 'pages/Checkout'
+import EditProfile from 'pages/EditProfile'
 import Login from 'pages/Login'
 import Logout from 'pages/Logout'
 import Main from 'pages/Main'
 import MovieCreate from 'pages/MovieCreate'
 import NotFound from 'pages/NotFound'
+import OrderConfirmation from 'pages/OrderConfirmation'
+import OrderSummary from 'pages/OrderSummary'
 import Register from 'pages/Register'
 import RegisterConfirm from 'pages/RegisterConfirm'
 import type { RouteObject } from 'react-router-dom'
@@ -20,6 +24,10 @@ const children: RouteObject[] = [
   {
     path: '/register',
     element: <Register />
+  },
+  {
+    path: '/editProfile',
+    element: <EditProfile />
   },
   {
     path: '/register/confirm',
@@ -41,7 +49,16 @@ const children: RouteObject[] = [
   {
     path: '*',
     element: <NotFound />
-  }
+  },
+  {
+    path: '/OrderConfirmation',
+    element: <OrderConfirmation />
+  },
+  {
+    path: '/OrderSummary',
+    element: <OrderSummary />
+  },
+  { path: '/Checkout', element: <Checkout /> }
 ]
 
 export default children
