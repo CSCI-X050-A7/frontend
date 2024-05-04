a460a969d0ca288e0b242129ba93fd2f22429186
Boming Zhang <bz14520@uga.edu>
2024-05-04 06:22:13 -0400

    feat: showing

--- src/pages/MovieSelect/index.tsx
+++ src/pages/MovieSelect/index.tsx
@@ -1,44 +1,39 @@
+import { useRequest } from 'ahooks'
+import type { SchemaMovie } from 'client'
 import PageContainer from 'components/PageContainer'
+import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import { useNavigate, useParams } from 'react-router-dom'
+import Backend from 'utils/service'
 
 const Index: React.FC = () => {
   const navigate = useNavigate()
   const { movieId } = useParams()
 
-  const movies = [
+  const [movie, setMovie] = useState<SchemaMovie>({
+    id: '',
+    title: '',
+    category: '',
+    producer: '',
+    rating_code: '',
+    reviews: '',
+    director: '',
+    synopsis: '',
+    cast: '',
+    trailer_picture: '',
+    trailer_video: '',
+    show_time: ''
+  })
+  const { loading } = useRequest(
+    async () => Backend.movie.v1MoviesDetail(movieId ?? ''),
     {
-      title: 'Argylle',
-      director: 'Matthew Vaughn',
-      length: '2h 30m',
-      image:
-        'https://connect.gtcmovies.com/CDN/Image/Entity/FilmPosterGraphic/HO00003984', // Replace with actual image URL
-      showTimes: ['10:00 AM', '2:00 PM', '6:00 PM', '9:30 PM']
+      onSuccess: res => {
+        setMovie(res.data)
+      }
     }
-    // {
-    //   title: 'Bob Marley One Love',
-    //   director: 'Reinaldus Marcus ',
-    //   length: '2h',
-    //   image: 'https://connect.gtcmovies.com/CDN/Image/Entity/FilmPosterGraphic/HO00004017', // Replace with actual image URL
-    //   showTimes: ['11:00 AM', '3:45 PM', '8:00 PM'],
-    // },
-    // {
-    //   title: 'Madame Web',
-    //   director: 'S.J. Clarkson',
-    //   length: '1h 45m',
-    //   image: 'https://connect.gtcmovies.com/CDN/Image/Entity/FilmPosterGraphic/HO00004029', // Replace with actual image URL
-    //   showTimes: ['5:45 PM', '8:00 PM'],
-    // },
-    // {
-    //   title: 'Wonka',
-    //   director: 'Paul King',
-    //   length: '2h 15m',
-    //   image: 'https://connect.gtcmovies.com/CDN/Image/Entity/FilmPosterGraphic/HO00003932', // Replace with actual image URL
-    //   showTimes: ['1:30 PM', '6:45 PM', '7:45 PM' ],
-    // },
-  ]
-
+  )
+  const showTimes = ['1:30 PM', '6:45 PM', '7:45 PM'] // TODO: load real show times
   return (
     <PageContainer>
       <div className='text-center'>
@@ -46,22 +41,20 @@ const Index: React.FC = () => {
       </div>
 
       <div className='d-flex flex-wrap justify-content-around'>
-        {movies.map((movie, index) => (
-          <Card key={index} style={{ width: '18rem', margin: '10px' }}>
+        {loading ? null : (
+          <Card style={{ width: '18rem' }}>
             <Card.Img
               variant='top'
-              src={movie.image}
+              src={movie.trailer_picture}
               alt={`${movie.title} poster`}
             />
             <Card.Body>
               <Card.Title>{movie.title}</Card.Title>
               <Card.Text>
                 <strong>Director:</strong> {movie.director}
-                <br />
-                <strong>Length:</strong> {movie.length}
               </Card.Text>
               <div>
-                {movie.showTimes.map((time, timeIndex) => (
+                {showTimes.map((time, timeIndex) => (
                   <Button
                     key={timeIndex}
                     variant='primary'
@@ -76,7 +69,7 @@ const Index: React.FC = () => {
               </div>
             </Card.Body>
           </Card>
-        ))}
+        )}
       </div>
     </PageContainer>
   )
