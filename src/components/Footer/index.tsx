ddee5410c65c5dccd0c746e4790a0f2afc3ba994
Boming Zhang <bz14520@uga.edu>
2024-05-07 15:34:01 -0400

    chore: update footer text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,9 +2,12 @@ import Container from 'react-bootstrap/Container'
 
 const Index: React.FC = () => (
   <footer className='footer mt-auto py-3 bg-body-tertiary'>
-    <Container fluid>
+    <Container
+      fluid
+      className='d-flex justify-content-center align-items-center'
+    >
       <span className='text-body-secondary'>
-        Place sticky footer content here.
+        CSCI X050 A7 - Cinema E-Booking System
       </span>
     </Container>
   </footer>
